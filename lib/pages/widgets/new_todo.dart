d1ac60eb736e39ad68b5c019cbfce1e85801f7a9
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-07-15 14:18:17 +0300

    Create NewTodo widget

---
+++ lib/pages/widgets/new_todo.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app_with_riverpod_sync/pages/providers/todo_list/todo_list_provider.dart';
+
+class NewTodo extends ConsumerStatefulWidget {
+  const NewTodo({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<NewTodo> createState() => _NewTodoState();
+}
+
+class _NewTodoState extends ConsumerState<NewTodo> {
+  final newTodoController = TextEditingController();
+
+  @override
+  void dispose() {
+    newTodoController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: newTodoController,
+      decoration: const InputDecoration(
+        labelText: 'What to do?',
+      ),
+      onSubmitted: (String description) {
+        if (description.trim().isNotEmpty) {
+          ref.read(todoListProvider.notifier).addTodo(description);
+          newTodoController.clear();
+        }
+      },
+      );
+  }
+}
