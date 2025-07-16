1fc3d546aef2ee97e76c588f09f5ac4e95e62d30
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-07-16 17:07:15 +0300

    Edit TodoItem

--- lib/pages/widgets/show_todos.dart
+++ lib/pages/widgets/show_todos.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app_with_riverpod_sync/pages/providers/filtered_todos/filtered_todos_provider.dart';
+import 'package:todo_app_with_riverpod_sync/pages/providers/todo_item/todo_item_provider.dart';
 import 'package:todo_app_with_riverpod_sync/pages/widgets/todo_item.dart';
 
 class ShowTodos extends ConsumerWidget{
@@ -10,12 +11,18 @@ class ShowTodos extends ConsumerWidget{
   Widget build(BuildContext context, WidgetRef ref) {
 
     final filteredTodos = ref.watch(filteredTodoProvider);
+    
     return ListView.separated(
       itemCount: filteredTodos.length,
       separatorBuilder: (BuildContext context, int index) => const Divider(color: Colors.grey),
       itemBuilder: (BuildContext context, int index) {
         final todo = filteredTodos[index];
-        return TodoItem(todo: todo);
+        return ProviderScope(
+          overrides: [
+            todoItemProvider.overrideWithValue(todo)
+          ],
+          child: const TodoItem(),
+          );
       },
     );
   }
